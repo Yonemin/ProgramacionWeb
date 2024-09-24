7cf2fb44c5d41d14626675be914c4848c318ca2f
Yonemin <frodriguezt2200@alumno.ipn.mx>
2024-09-24 17:55:18 -0600

    creación de directorio y archivo

---
+++ script.js
@@ -0,0 +1,26 @@
+//CLASE 24/09/24
+//NOMBRE: Rodríguez Torres Fernanda
+
+/*
+Variables:
+*/
+
+let nombre = "Juan";
+const PI = 3.1416;
+var edad = 25;
+
+alert("Hola, clase 5IV7 desde el JS externo");
+
+//operadores
+let a = 5;
+let b = 2;
+let suma = a + b;
+let resta = a - b;
+let multiplicacion = a*b;
+let division = a/b;
+
+//operadores de comparación:
+a>b;    //T
+a<b;    //F
+a == b; //F
+a === b;    //F
