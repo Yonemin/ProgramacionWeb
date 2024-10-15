9beb5b88c1b9b795c0cca9b55ce6350b0136ec56
Yonemin <160188128+Yonemin@users.noreply.github.com>
2024-10-15 17:54:30 -0600

    JS

---
+++ Clase_10102024/script.js
@@ -0,0 +1,16 @@
+//comentario de una línea
+/*
+Comentario
+multi
+línea
+*/
+
+//declaramos nuestra función
+function hello(){
+    return 'hola js';
+}
+
+console.log(hello());
+
+/*Investigar diferentes usos de funciones en JS
+y ejemplificar cada una de ellas. Como con uno de parámetros, función dentro de otra función*/
---
+++ Clase_15102024/script.js
@@ -0,0 +1,48 @@
+//definición de una función
+/*
+function hello(){
+    console.log('Hola clase');
+    console.log(' 5IV7');
+}
+
+//Llamada a la función
+hello();
+hello();
+hello();
+hello();
+hello();
+
+
+//Retorno de una función
+function hello(){
+    return('Hola Clase');
+}
+
+const result = hello();
+
+console.log(result)
+
+
+//función que retorna otra función
+function hello(){
+    return function(){
+        return('Hola, soy la función 2');
+    };
+}
+
+console.log(hello()())
+
+
+//función con parámetros
+function add(x){
+    console.log(x);
+}
+
+add(5);
+*/
+
+function add(x,y){
+    console.log(x+y)
+}
+
+add(5,3);
