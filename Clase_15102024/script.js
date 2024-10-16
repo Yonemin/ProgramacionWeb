7c4512ff9db048b9c3fd69359891c0e2773fa16b
Yonemin <160188128+Yonemin@users.noreply.github.com>
2024-10-15 18:29:03 -0600

    Ejercicio terminado por medio punto extra

--- Clase_15102024/script.js
+++ Clase_15102024/script.js
@@ -39,10 +39,59 @@ function add(x){
 }
 
 add(5);
-*/
 
+//multiparámetros
 function add(x,y){
+
     console.log(x+y)
 }
 
 add(5,3);
+
+
+//control de error en múltiples parámetros 
+function add(x,y){      //o y=0
+
+    if(y===undefined){
+        y=0
+    }
+    console.log(x+y)
+}
+
+add(8,);
+
+
+//parámetro de tipo cadena
+function add(name){
+    console.log('Hola '+name)
+}
+
+add("Clase JS")
+*/
+
+//Objetos
+const user = {
+    nombre:'Denji',
+    apellidoP:'Hayawaka',
+    apellidoM:'Fujimoto',
+    edad:26,
+    direccion:{
+        calle:'Yokohama',
+        numero:696,
+        colonia:'Shoujo',
+        delegacion:'Katakana'
+    },
+    amigos:['PowerPoint','Aki47'],
+    activo:true
+}
+
+//En consola: nombre, apellidos, edad, amigos
+//En un alert: si está activo, calle, número, colonia, delegación
+
+console.log(user.nombre, user.apellidoP, user.apellidoM, user.edad, user.amigos[0], user.amigos[1])
+if(activo=true){
+    alert('Está activo')
+}else{
+    alert('No está activo')
+}
+alert(user.direccion.calle+' '+user.direccion.numero+' '+user.direccion.colonia+' '+user.direccion.delegacion)
