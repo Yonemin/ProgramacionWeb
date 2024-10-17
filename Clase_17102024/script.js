d5e5464f718976baf3aaa2d4fc70643dad9d6f97
Yonemin <160188128+Yonemin@users.noreply.github.com>
2024-10-17 15:52:31 -0600

    Elementos HTML desde el JS

---
+++ Clase_17102024/script.js
@@ -0,0 +1,10 @@
+document.title = "Clase de JS";
+console.log(document.title);
+
+const h1 = document.createElement("h1");
+h1.textContent = "Mi texto desde JS";
+document.body.appendChild(h1);
+
+const texto = document.createElement("p");
+texto.textContent = "17102024"
+document.body.appendChild(texto);
